4ee6f4934cdcd1827332054a2389cf5fdb415bfc
Austin Engle <engleau@gmail.com>
2022-01-02 12:00:30 -0500

    update to seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,20 @@
-use employees_db;
+use employees_db
 
-SELECT * FROM employee
-JOIN role on role.id = employee.role_id
+
+INSERT INTO department
+(name)
+VALUES
+('Engineering')
+('Sales')
+('Legal')
+('Finance')
+
+INSERT INTO role
+(title, salary, department_id)
+VALUES
+('Lead Engineer', 150000, 1)
+('Engineer', 125000, 1)
+('Lead Sales', 100000, 2)
+('Sales Agent', 75000, 2)
+('HR Manger', 95000, 3)
+('HR', 50000, 3)
