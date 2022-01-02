928d1fe90dc50171d378c0ee696c0af17026c50e
Austin Engle <engleau@gmail.com>
2022-01-02 08:33:50 -0500

    final update without readme

--- db/query.sql
+++ db/query.sql
@@ -1,6 +1,11 @@
 USE employees_db;
-SELECT * FROM employee
-LEFT JOIN role 
-ON role.id = employee.role_id
-LEFT JOIN employee AS manager
-ON employee.manager_id = employee.id;
+
+SELECT 
+CONCAT(employee.first_name," ",employee.last_name) AS employee_name,
+CONCAT(manager.first_name," ",manager.last_name) AS manager_name,
+role.title AS role
+FROM employee manager
+RIGHT JOIN employee e
+ON manager.id = employee.manager_id
+INNER JOIN role r
+ON role.id = employee.role_id;
--- server.js
+++ server.js
@@ -6,33 +6,46 @@ const db = mysql.createConnection(
     user: "root",
     database: "employees_db",
   },
-  'Connected to the employees_db database.'
+  "Connected to the employees_db database."
 );
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "Select Option",
-      name: "select",
-      choices: [
-        "view all employees",
-        "view all roles",
-        "view all departments",
-        "exit",
-      ],
-    },
-  ])
-  .then((answer) => {
-    switch (answer.selection) {
-      case "view all employees": {
-        db.query('SELECT * FROM employee')
+const start = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Select Option",
+        name: "select",
+        choices: [
+          "view all employees",
+          "view all roles",
+          "view all departments",
+          "exit",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.selection) {
+        case "view all employees":
+          {
+            db.query("SELECT * FROM employee");
+          }
+          break;
+
+        case "view all roles":
+          {
+            db.query("SELECT * FROM role");
+          }
+          break;
+        case "view all departments":
+          {
+            db.query("SELECT * FROM department");
+          }
+          break;
+
+        default:
+          return process.exit();
       }
-      case "view all roles": {
-      }
-      case "view all departments": {
-      }
-      default:
-        return process.exit();
-    }
-  });
+      return start();
+    });
+};
